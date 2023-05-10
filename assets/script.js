6801f989af7eb8262132575754da09f59ecadf73
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2023-05-10 15:26:27 +0530

    first commit

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+/*dashbord section*/
+$("#linkHome").click(function(){
+    $("#home").css("display","block");
+    $("#customer-section").css("display","none");
+    $("#item-section").css("display","none");
+    $("#placeorder-section").css("display","none");
+  
+});
+
+$("#linkCustomer").click(function(){
+    $("#home").css("display","none");
+    $("#customer-section").css("display","block");
+    $("#item-section").css("display","none");
+    $("#placeorder-section").css("display","none");
+  
+});
+
+
+
+$("#linkItem").click(function(){
+    $("#home").css("display","none");
+    $("#customer-section").css("display","none");
+    $("#item-section").css("display","block");
+    $("#placeorder-section").css("display","none");
+  
+});
+
+
+$("#linkplace").click(function(){
+    $("#home").css("display","none");
+    $("#customer-section").css("display","none");
+    $("#item-section").css("display","none");
+    $("#placeorder-section").css("display","block");
+  
+});
