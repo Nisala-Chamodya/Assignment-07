4044b1abd4772f38f707f486a81d0205f0699509
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2023-06-15 13:21:47 +0530

    Upgrade UI/UX & DashBoard

--- controller/DashBoardController.js
+++ controller/DashBoardController.js
@@ -1,63 +1,70 @@
-import {Order} from "../models/Order.js";
-import {getAllDB} from "../db/DB.js";
+import { Order } from "../models/Order.js";
+import { getAllDB } from "../db/DB.js";
 
 export class DashBoardController {
+  constructor() {
+    this.handleTableLoad();
+    this.handleLabelData();
+  }
 
-    constructor() {
-        this.handleTableLoad();
-        this.handleLabelData();
-    }
+  handleTableLoad() {
+    getAllDB("ORDER").map((value) => {
+      let count = value._itemArray.length;
+      let total = 0;
+      for (let i = 0; i < count; i++) {
+        total += value._itemArray[i]._total;
+      }
+      var row =
+        "<tr>" +
+        "<td>" +
+        value._orderId +
+        "</td>" +
+        "<td>" +
+        value._customer._id +
+        "</td>" +
+        "<td>" +
+        value._customer._name +
+        "</td>" +
+        "<td>" +
+        total +
+        "</td>" +
+        "<td>" +
+        value._orderDate +
+        "</td>" +
+        "<td><div>Payed</div></td>" +
+        "</tr>";
 
-    handleTableLoad() {
+      $("#orderDetailTbl tbody").prepend(row);
+    });
+  }
 
-        getAllDB("ORDER").map(value => {
+  handleLabelData() {
+    $("#totalCustomer").text(getAllDB("DATA").length);
 
-            let count = value._itemArray.length;
-            let total = 0;
-            for (let i = 0; i < count; i++) {
+    let count = 0;
+    var todayIncome = 0;
 
-                total += value._itemArray[i]._total;
-            }
-            var row = "<tr>" +
-                "<td>" + value._orderId + "</td>" +
-                "<td>" + value._customer._id + "</td>" +
-                "<td>" + value._customer._name + "</td>" +
-                "<td>" + total + "</td>" +
-                "<td>" + value._orderDate + "</td>" +
-                "<td><div>Payed</div></td>" +
-                "</tr>";
+    var date = new Date();
+    var nowDate =
+      date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
 
-            $('#orderDetailTbl tbody').prepend(row);
-        });
-    }
-
-    handleLabelData() {
-
-        $('#totalCustomer').text(getAllDB("DATA").length);
-
-        let count = 0;
-        var todayIncome = 0;
-
-        var date = new Date();
-        var nowDate = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
-
-        getAllDB("ORDER").map(value => {
-            if (value._orderDate === nowDate) {
-                for (let i = 0; i < value._itemArray.length; i++) {
-                    console.log(value._itemArray[i]._total);
-                    todayIncome += parseInt(value._itemArray[i]._total)
-                }
-                count++;
-            }
-        });
-        $('#todayOrders').text(count);
-        $('#todayIncome').text("Rs  " + todayIncome);
-    }
+    getAllDB("ORDER").map((value) => {
+      if (value._orderDate === nowDate) {
+        for (let i = 0; i < value._itemArray.length; i++) {
+          console.log(value._itemArray[i]._total);
+          todayIncome += parseInt(value._itemArray[i]._total);
+        }
+        count++;
+      }
+    });
+    $("#todayOrders").text(count);
+    $("#todayIncome").text("Rs  " + todayIncome);
+  }
 }
 
 export function handleRefreshAll() {
-    dashBoardController.handleLabelData();
-    dashBoardController.handleTableLoad();
+  dashBoardController.handleLabelData();
+  dashBoardController.handleTableLoad();
 }
 
 let dashBoardController = new DashBoardController();
